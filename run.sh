2fedc10e7c4366415c946335e42deb2a2357dc72
Joseph Cordara <josephcordara@gmail.com>
2022-09-14 22:03:15 -0600

    Add debug flags for compilation

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir -p bin
-g++ main/*.cpp lib/*.cpp -o bin/cpu_raytracer.exe && ./bin/cpu_raytracer
+g++ -D_DEBUG -g main/*.cpp lib/*.cpp -o bin/cpu_raytracer.exe && ./bin/cpu_raytracer
--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir -p bin/tests
-g++ tests/test_main.cpp lib/*.cpp -o bin/tests/cpu_raytracer_tests.exe && ./bin/tests/cpu_raytracer_tests
+g++ -D_DEBUG -g tests/test_main.cpp lib/*.cpp -o bin/tests/cpu_raytracer_tests.exe && ./bin/tests/cpu_raytracer_tests

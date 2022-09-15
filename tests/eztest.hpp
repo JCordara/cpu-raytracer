a0ab81c0a6bb10dc116f1535f35bab635c242856
Joseph Cordara <josephcordara@gmail.com>
2022-09-14 18:30:17 -0600

    Align and color test output

--- tests/eztest.hpp
+++ tests/eztest.hpp
@@ -13,28 +13,56 @@ namespace eztest {
     struct Test {
         TestFunc func;
         const char* name;
+        int name_len;
     };
 
     std::vector<Test> tests;
 
     void add_test(TestFunc f, const char* test_name) {
+        // Get test_name length
+        const char *p = test_name;
+        int len = 0;
+        while (*p != '\0') {
+            p++;
+            len++;
+        }
+
         Test t;
         t.func = f;
         t.name = test_name;
+        t.name_len = len;
         tests.push_back(t);
     }
 
     bool test_main() {
-        all_passed = true;
-
+        // Used for aligning output
+        int max_name_len = 0;
         for (Test t : tests) {
-            std::cout << "\nRunning test: " << t.name << " ... ";
-            bool test_passed = t.func();
-            std::cout << (test_passed ? "Pass" : "\n-- TEST FAILED\n");
-            all_passed = all_passed && test_passed;
+            if (t.name_len > max_name_len) {
+                max_name_len = t.name_len;
+            }
         }
 
-        std::cout << "\n\n" << (all_passed ? "All tests passed!" : "Tests failed.") << "\n";
+        all_passed = true;
+        for (Test t : tests) {
+            std::cout << "\nRunning test: " << t.name << " ";
+            bool test_passed = t.func();
+            all_passed = all_passed && test_passed;
+
+            // Align output by padding with '.'s
+            if (test_passed) {
+                for (int i = 0; i - 3 < max_name_len - t.name_len; i++) {
+                    std::cout << ".";
+                }
+            }
+            std::cout << (test_passed 
+                ? " \033[0;32mPass\033[0m" 
+                : "\n-- \033[0;31mTEST FAILED\033[0m\n");
+        }
+
+        std::cout << "\n\n" << (all_passed 
+            ? "\033[0;32mAll tests passed!\033[0m" 
+            : "\033[0;31mTests failed.\033[0m") << "\n";
         return all_passed;
     }
 };

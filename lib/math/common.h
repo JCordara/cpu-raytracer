d21d71c51f8d5ffb03ffc4ec5dbb729bc300ad9b
Joseph Cordara <josephcordara@gmail.com>
2022-09-17 21:04:35 -0600

    Add custom trig functions

--- lib/math/common.h
+++ lib/math/common.h
@@ -1,7 +1,80 @@
 #ifndef COMMON_MATH_H
 #define COMMON_MATH_H
 
-#define NaN 1.175494351e-38f
+#define NaN           1.175494351e-38f
+#define QUARTER_PI    0.7853981633974f
+#define HALF_PI       1.5707963267948f
+#define PI            3.1415926535897f
+#define THREE_HALF_PI 4.7123889803846f
+#define TWO_PI        6.2831853071795f
+
+constexpr float _rad_to_deg_ratio = 180.0f / PI;
+constexpr float _deg_to_rad_ratio = PI / 180.0f;
+constexpr float _2_fact_inv = 1.0f / (2.0f);
+constexpr float _3_fact_inv = 1.0f / (3.0f * 2.0f);
+constexpr float _4_fact_inv = 1.0f / (4.0f * 3.0f * 2.0f);
+constexpr float _5_fact_inv = 1.0f / (5.0f * 4.0f * 3.0f * 2.0f);
+constexpr float _6_fact_inv = 1.0f / (6.0f * 5.0f * 4.0f * 3.0f * 2.0f);
+constexpr float _7_fact_inv = 1.0f / (7.0f * 6.0f * 5.0f * 4.0f * 3.0f * 2.0f);
+
+inline constexpr float radians(float degrees) {
+    return _deg_to_rad_ratio * degrees;
+}
+
+inline constexpr float degrees(float radians) {
+    return _rad_to_deg_ratio * radians;
+}
+
+inline constexpr float _sin_impl(float radians) {
+    float pow3 = radians * radians * radians;
+    float pow5 = pow3 * radians * radians;
+    float pow7 = pow5 * radians * radians;
+    return radians - (pow3 * _3_fact_inv) + (pow5 * _5_fact_inv) - (pow7 * _7_fact_inv);
+}
+
+inline constexpr float _cos_impl(float radians) {
+    float pow2 = radians * radians;
+    float pow4 = pow2 * radians * radians;
+    float pow6 = pow4 * radians * radians;
+    return 1 - (pow2 * _2_fact_inv) + (pow4 * _4_fact_inv) - (pow6 * _6_fact_inv);
+}
+
+inline float sin(float radians) {
+    // Get into 0 - 360 degree range
+    while (radians >= TWO_PI) radians -= TWO_PI;
+    while (radians < 0) radians += TWO_PI;
+
+    // Use vertical symmetry if in quadrants 3 or 4
+    float multiplier = 1.0f;
+    if (radians >= PI) {
+        radians -= PI;
+        multiplier = -1.0f;
+    }
+
+    if (radians >= HALF_PI) radians = PI - radians;
+    if (radians < QUARTER_PI) return _sin_impl(radians) * multiplier;
+    else return _cos_impl(HALF_PI - radians) * multiplier;
+}
+
+inline constexpr float cos(float radians) {
+    // Get into 0 - 360 degree range
+    while (radians >= TWO_PI) radians -= TWO_PI;
+    while (radians < 0) radians += TWO_PI;
+
+    // Use vertical symmetry if in quadrants 3 or 4
+    float multiplier = 1.0f;
+    if (radians >= PI) {
+        radians -= PI;
+        multiplier = -1.0f;
+    }
+
+    if (radians >= HALF_PI) {
+        radians = PI - radians;
+        multiplier *= -1.0f;
+    }
+    if (radians < QUARTER_PI) return _cos_impl(radians) * multiplier;
+    else return _sin_impl(HALF_PI - radians) * multiplier;
+}
 
 inline constexpr float _sqrt_impl(float x, float s, float s2)
 {
--- tests/test_common_math.hpp
+++ tests/test_common_math.hpp
@@ -3,14 +3,36 @@
  * Using custom eztest framework, see eztest_macros.h for implementation detail
  */
 
+TEST_START(test_sin)
+
+    CHECK_EQ_EPS(sin(radians(30)),    0.5f,      "Quadrant 1 incorrect")
+    CHECK_EQ_EPS(sin(radians(104)),   0.970295f, "Quadrant 2 incorrect")
+    CHECK_EQ_EPS(sin(radians(255)),  -0.965925f, "Quadrant 3 incorrect")
+    CHECK_EQ_EPS(sin(radians(300)),  -0.866025f, "Quadrant 4 incorrect")
+    CHECK_EQ_EPS(sin(radians(390)),   0.5f,      "Quadrant 1 incorrect - above 360")
+    CHECK_EQ_EPS(sin(radians(824)),   0.970295f, "Quadrant 2 incorrect - way above 360")
+    CHECK_EQ_EPS(sin(radians(-825)), -0.965926f, "Quadrant 3 incorrect - way below 0")
+    CHECK_EQ_EPS(sin(radians(-60)),  -0.866025f, "Quadrant 4 incorrect - below 0")
+
+TEST_END(test_sin)
+
+TEST_START(test_cos)
+
+    CHECK_EQ_EPS(cos(radians(30)),    0.866025f, "Quadrant 1 incorrect")
+    CHECK_EQ_EPS(cos(radians(104)),  -0.241922f, "Quadrant 2 incorrect")
+    CHECK_EQ_EPS(cos(radians(255)),  -0.258819f, "Quadrant 3 incorrect")
+    CHECK_EQ_EPS(cos(radians(300)),   0.5f,      "Quadrant 4 incorrect")
+    CHECK_EQ_EPS(cos(radians(390)),   0.866025f, "Quadrant 1 incorrect - above 360")
+    CHECK_EQ_EPS(cos(radians(824)),  -0.241922f, "Quadrant 2 incorrect - way above 360")
+    CHECK_EQ_EPS(cos(radians(-825)), -0.258818f, "Quadrant 3 incorrect - way below 0")
+    CHECK_EQ_EPS(cos(radians(-60)),   0.5f,      "Quadrant 4 incorrect - below 0")
+
+TEST_END(test_cos)
+
 TEST_START(test_sqrt)
 
-    float result;
-
-    result = sqrt(4.0f);
-    CHECK_EQ(result, 2.0f, "Square root incorrect (case: 4.0)")
-
-    result = sqrt(-1.0f);
-    CHECK_EQ(result, NaN, "Square root incorrect (case: -1.0)")
+    CHECK_EQ_EPS(sqrt(4.0f), 2.0f, "Square root incorrect (case: 4.0)")
+    CHECK_EQ_EPS(sqrt(76851123.861123f), 8766.477277f, "Square root incorrect (case: 76851123.861123)")
+    CHECK_EQ_EPS(sqrt(-1.0f), NaN, "Square root incorrect (case: -1.0)")
 
 TEST_END(test_sqrt)

8f86bcae24ec679e9b6f5fab88a7eb9f1dc3f157
Joseph Cordara <josephcordara@gmail.com>
2022-09-18 00:15:07 -0600

    Add debug output for testing

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -3,8 +3,25 @@
 
 #include "../lib/vex3d.h"
 
+void print_mat3(const mat3& m, bool newline = true) {
+    std::cout << "| " << m[0][0] << ", " << m[0][1] << ", " << m[0][2];
+    std::cout << (newline ? " |\n" : ", ");
+    std::cout << "| " << m[1][0] << ", " << m[1][1] << ", " << m[1][2];
+    std::cout << (newline ? " |\n" : ", ");
+    std::cout << "| " << m[2][0] << ", " << m[2][1] << ", " << m[2][2];
+    std::cout << (newline ? " |\n" : " ");
+}
+
+void print_vec3(const vec3& v, bool newline = true) {
+    std::cout << "(" << v.x << ", " << v.y << ", " << v.z << ") ";
+    if (newline) {
+        std::cout << "\n";
+    }
+}
+
 int main() {
-    #include "test_vec3f.hpp"
     #include "test_common_math.hpp"
+    #include "test_vec3f.hpp"
+    #include "test_mat3.hpp"
     return eztest::test_main();
 }

42c93ba2e67dc4bb897e53cc5b95a6aa0c937343
Joseph Cordara <josephcordara@gmail.com>
2022-09-24 14:33:30 -0600

    Add optional class for use with intersections

---
+++ lib/optional.h
@@ -0,0 +1,77 @@
+#ifndef OPTIONAL_H
+#define OPTIONAL_H
+
+/**
+ * opt (optional, shortened for ease of typing) objects are used to pass around 
+ * nullable objects. They provide explicit boolean conversion and access to the
+ * underlying object through a reference. The contained type must implement the
+ * copy constructor and assignment operator.
+ */
+template<typename T>
+class opt {
+private:
+    
+    T* _value;
+
+public:
+
+    opt() : _value(nullptr) {}
+
+    opt(const T& value) {
+        _value = new T;
+        *_value = value; // copy ctor
+    }
+
+    opt(T&& value) {
+        _value = new T;
+        *_value = value; // copy ctor
+    }
+
+    opt(const opt<T>& other) {
+        _value = new T;
+        *_value = *other._value; // copy ctor
+    }
+
+    opt(opt<T>&& other) {
+        _value = new T;
+        *_value = *other._value; // copy ctor
+    }
+
+    opt<T>& operator=(const opt<T>& rhs) {
+        if (this != &rhs) {
+            if (_value != nullptr) delete _value;
+            _value = new T;
+            *_value = *rhs._value;
+        }
+        return *this;
+    }
+
+    opt<T>& operator=(opt<T>&& rhs) {
+        if (_value != nullptr) delete _value;
+        _value = new T;
+        *_value = *rhs._value;
+        return *this;
+    }
+
+    ~opt() {
+        delete _value;
+    }
+
+    T& unwrap() {
+        return *_value;
+    }
+
+    T* ptr() {
+        return _value;
+    }
+
+    explicit operator bool() const {
+        return (_value != nullptr);
+    }
+
+    static opt<T> none() {
+        return opt<T>();
+    }
+};
+
+#endif // OPTIONAL_H

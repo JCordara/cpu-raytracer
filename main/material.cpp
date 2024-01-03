97326fa98954c3f567c7ced300d4fabd55376e5b
Joseph Cordara <josephcordara@gmail.com>
2024-01-03 13:40:13 -0700

    Add transparency and refractive index to materials

--- main/material.cpp
+++ main/material.cpp
@@ -3,21 +3,32 @@
 
 Material::Material() :
     _color(255, 255, 255),
-    _reflectivity(0.5f)
+    _reflectivity(0.5f),
+    _opacity(1.0f),
+    _refractive_index(1.0f)
 {
     print("Material default ctor\n");
 }
 
 Material::Material(const vec3& color) :
-    _color(color),
-    _reflectivity(0.5f)
+    Material(color, 0.0f, 1.0f, 1.0f)
 {
     print("Material color ctor\n");
 }
 
 Material::Material(const vec3& color, const float reflectivity) :
+    Material(color, reflectivity, 1.0f, 1.0f)
+{}
+
+Material::Material(const vec3& color, const float reflectivity, const float opacity) :
+    Material(color, reflectivity, opacity, 1.0f)
+{}
+
+Material::Material(const vec3& color, const float reflectivity, const float opacity, const float refractive_index) :
     _color(color),
-    _reflectivity(reflectivity)
+    _reflectivity(reflectivity),
+    _opacity(opacity),
+    _refractive_index(refractive_index)
 {}
 
 
@@ -27,4 +38,12 @@ const vec3& Material::color() const {
 
 const float Material::reflectivity() const {
     return this->_reflectivity;
+}
+
+const float Material::opacity() const {
+    return this->_opacity;
+}
+
+const float Material::refractive_index() const {
+    return this->_refractive_index;
 }
--- main/material.h
+++ main/material.h
@@ -9,15 +9,21 @@ private:
 
     vec3 _color;
     float _reflectivity;
+    float _opacity;
+    float _refractive_index;
 
 public:
 
     Material();
     Material(const vec3& color);
     Material(const vec3& color, const float reflectivity);
+    Material(const vec3& color, const float reflectivity, const float opacity);
+    Material(const vec3& color, const float reflectivity, const float opacity, const float recraftice_index);
 
     const vec3& color() const;
     const float reflectivity() const;
+    const float refractive_index() const;
+    const float opacity() const;
 
 };
 
